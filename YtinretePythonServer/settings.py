1c895164b0e05c7223fd91b5743b19d124fd6e86
lirui2015 <lirui2015@corp.netease.com>
2017-03-03 17:05:41 +0800

    give up...

--- YtinretePythonServer/settings.py
+++ YtinretePythonServer/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '???'
+SECRET_KEY = '9AC1D06AE53FE25FADBF6399D82D0683'
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.ytinrete.com', 'localhost']
+ALLOWED_HOSTS = ['www.ytinrete.com', 'localhost', '10.234.98.128']
+
 
 
 # Application definition
