a7829561f7a47b9aacf665325c2c34c7a0ab7f2e
lirui2015 <lirui2015@corp.netease.com>
2017-03-10 19:45:32 +0800

    use another library

--- Tools/TestHTTPClient.py
+++ Tools/TestHTTPClient.py
@@ -2,6 +2,8 @@ import math
 import requests
 import threading
 from bs4 import BeautifulSoup
+from urllib import request as req
+import zlib
 
 
 def get(url, params=None, headers=None):
@@ -10,6 +12,25 @@ def get(url, params=None, headers=None):
         print(r.text)
     except BaseException as e:
         print(e)
+
+
+def req_maker(path):
+    if path:
+        reqs = req.Request(path)
+        reqs.add_header("User-Agent",
+                       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+        reqs.add_header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+        reqs.add_header("Accept-Encoding", "gzip, deflate, sdch")
+        reqs.add_header("Accept-Language", "zh-CN,zh;q=0.8,en;q=0.6")
+        return reqs
+    else:
+        return None
+
+
+def get_response_str(reqs):
+    with req.urlopen(reqs, timeout=5) as f:
+        decompressed_data = zlib.decompress(f.read(), 16 + zlib.MAX_WBITS)
+        return str(decompressed_data, "utf-8", errors='replace')
 
 
 def post(url, data=None, params=None, headers=None, files=None):
@@ -72,14 +93,14 @@ def page():
 def test_thread(data):
     if data and data.get('Addr'):
         try:
-            r = requests.get('http://whatismyipaddress.com/ip/' + data.get('Addr'))
-            if r.status_code == 200:
-                html_tree = BeautifulSoup(r.text, 'lxml')
-                for meta in html_tree.head.select('meta'):
-                    if meta.get('name') == 'description':
-                        # print(meta.get('content'))
-                        location = meta.get('content')
-                        break
+            html_str = get_response_str(req_maker('http://whatismyipaddress.com/ip/' + data.get('Addr')))
+            html_tree = BeautifulSoup(html_str, 'lxml')
+            for meta in html_tree.head.select('meta'):
+                if meta.get('name') == 'description':
+                    print(meta.get('content'))
+                    location = meta.get('content')
+                    break
+
         except BaseException as e:
             print(e)
 
@@ -90,6 +111,7 @@ if __name__ == "__main__":
     # get("http://localhost:8000/python/TestOne")
 
     # post()
+
 
     threading.Thread(target=test_thread, args=[{'Addr': '140.205.201.6'}]).start()
 
